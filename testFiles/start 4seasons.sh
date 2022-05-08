d2028cea667383ca69fbd4f0796cdbe0c2100179
Krzysztof Para <k.para@contina.pl>
2022-05-08 16:54:03 +0200

    tests

---
+++ testFiles/start 4seasons.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./GenerativeArt -i 4seasons.png -t 16 -p 512 -s 1028 -S 111 -r 0.98 --hours 4 --scale 0.3 -m 0.001
+./GenerativeArt -i gustawK.png -t 16 -p 512 -s 1028 -S 111 -r 0.98 --hours 4 --scale 0.3 -m 0.001
---
+++ testFiles/startbaletnice.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./GenerativeArt -i baletnice.png -t 16 -p 512 -s 512 -S 111 -r 0.98 --hours 4 --scale 0.3
+./GenerativeArt -i baletnice.png -t 16 -p 256 -s 2048 -S 111 -r 0.98 --hours 4 --scale 0.2
